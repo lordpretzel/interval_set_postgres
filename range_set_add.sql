14708b8c70a07d82fa66debb0192a6cfecc66f51
dejazzhands <sameffendy08@gmail.com>
2024-11-21 00:28:56 -0600

    adding two range functions

---
+++ range_set_add.sql
@@ -0,0 +1,23 @@
+ CREATE OR REPLACE FUNCTION range_set_add(l int[][], r int[][])
+ RETURNS int[][] AS $$
+    result int[][];
+    i int;
+    j int;
+BEGIN
+    -- Initialize result array
+    result := '{}';
+
+    -- double for loop to iterate throuh all elements of l and r
+    FOR i IN array_lower(l, 1)..array_upper(l, 1) LOOP
+        FOR j IN array_lower(r, 1)..array_upper(r, 1) LOOP
+        
+            result := result || ARRAY[ARRAY[
+                    l[i][1] + r[j][1],
+                    l[i][2] + r[j][2]
+                ]];
+        END LOOP;
+    END LOOP;
+
+    RETURN result;
+END;
+$$ LANGUAGE plpgsql;
